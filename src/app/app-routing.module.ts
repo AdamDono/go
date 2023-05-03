b9a47adcd1b2362476ed6902070daf325baf734f
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-05-03 12:52:09 +0200

    rhufkrfvkd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+import { OtpComponent } from './otp/otp.component';
+import { SignupComponent } from './signup/signup.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
+  {path: '', component: SignInComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'Otp', component: OtpComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { PasswordComponent } from './password/password.component';
+import { OtpComponent } from './otp/otp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SignInComponent,
+    PasswordComponent,
+    OtpComponent
   ],
   imports: [
     BrowserModule,
